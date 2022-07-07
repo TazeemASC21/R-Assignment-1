eb5cd23c317db18a8cf7006502dc7aa6a19ecae6
TazeemASC21 <87765937+TazeemASC21@users.noreply.github.com>
2022-07-07 18:03:03 -0400

    completed assignment

--- assignment1.R
+++ assignment1.R
@@ -2,27 +2,27 @@
 
 
 # Create a variable 'x' that holds a value of 9
-
+9 -> x
 # Create a variable 'y' that holds a value of x
-
+x -> y
 # Create a variable 'z' that holds the sum of x and y
-
+x + y -> z
 # Clear the variable x from the workspace
-
+rm(x)
 # Clear all variables from the workspace
-
+rm(list=ls())
 # Make a statement that directs to http://www.google.com
-
+browseURL('http://www.google.com')
 # Show a list of all packages installed
-
+library()
 # Show a list of packages currently installed and usable
-
-
-
+search()
 # Magic Number Exercise
 # Do the Following:
     # Choose any number and add 2 to it.
     # Multiply the result by 3.
     # Subtract 6 from the answer.
-    # Divide what you get by 3.
+    # Divide wshat you get by 3.
 # you should end up with the same number that you started with
+
+((42+ 2)*3 - 6) /3
